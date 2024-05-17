26406811c7562ddc222144b450e628b4ae421905
boa50 <bruno.albuquerque50@gmail.com>
2024-05-17 11:49:41 -0300

    Added types for the axis component

--- index.d.ts
+++ index.d.ts
@@ -41,3 +41,29 @@ export function hideText(textNode: Element, progress?: number): null
 export function showText(textNode: Element, progress?: number): null
 
 export function changeText(textNode: Element, htmlText: string): null
+
+/*
+*   AXIS
+*/
+export function adjustColours(g: Element, colour: string, hideDomain?: boolean): null
+
+export function addAxis({ }: {
+    chart: Element, height: number, width: number, colour?: string,
+    x?: Function, xLabel?: string, xFormat?: Function, xTickValues?: Array, xNumTicks?: number, xNumTicksForceInitial?: boolean, hideXdomain?: boolean,
+    y?: Function, yLabel?: string, yFormat?: Function, yTickValues?: Array, yNumTicks?: number, yNumTicksForceInitial?: boolean, hideYdomain?: boolean,
+    yRight?: Function, yRightLabel?: string, yRightFormat?: Function, yRightTickValues?: Array, yRightNumTicks?: number, yRightNumTicksForceInitial?: boolean
+}): null
+
+export function updateXaxis({ }: {
+    chart: Element,
+    x: Function,
+    format?: Function,
+    tickValues?: Array
+}): null
+
+export function updateYaxis({ }: {
+    chart: Element,
+    y: Function,
+    format?: Function,
+    hideDomain?: boolean
+}): null
