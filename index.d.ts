59c7b031855d04bec808cf4be1f28a7a4b99b5ef
boa50 <bruno.albuquerque50@gmail.com>
2024-05-17 11:23:49 -0300

    new test

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,12 @@
+// animation/number.js
+export function createNumber({ }: {
+    svg: Element,
+    x: number,
+    y: number,
+    textColour: string,
+    fontSize: string,
+    alignVertical: 'auto' | 'middle'
+}): Element
+
 export function logThing(txt: string): null;
 export function adjustColours(g: any, colour: string, hideDomain: boolean): null;
